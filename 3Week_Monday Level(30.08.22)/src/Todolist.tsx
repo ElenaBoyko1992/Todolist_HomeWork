a540f51617d129337f1347eed343456b4de5b9ce
ElenaBoyko1992 <Bionka@inbox.ru>
2022-08-30 23:34:39 +0300

    30.08.2022

---
+++ 3Week_Monday Level(30.08.22)/src/Todolist.tsx
@@ -0,0 +1,46 @@
+import React, {ChangeEvent, useState} from 'react';
+import {FilterValuesType} from './App';
+
+type TaskType = {
+    id: string
+    title: string
+    isDone: boolean
+}
+
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+    removeTask: (taskId: string) => void
+    changeFilter: (value: FilterValuesType) => void
+}
+
+export function Todolist(props: PropsType) {
+    const [inputValue, setInputValue] = useState('');
+    return <div>
+        <h3>{props.title}</h3>
+        <div>
+            <input value={inputValue} onChange={(e: ChangeEvent<HTMLInputElement>)=> setInputValue(e.currentTarget.value) }/>
+            <button onClick={()=> alert('add!')}>+</button>
+        </div>
+        <ul>
+            {
+                props.tasks.map(t => <li key={t.id}>
+                    <input type="checkbox" checked={t.isDone}/>
+                    <span>{t.title}</span>
+                    <button onClick={ () => { props.removeTask(t.id) } }>x</button>
+                </li>)
+            }
+        </ul>
+        <div>
+            <button onClick={ () => { props.changeFilter("all") } }>
+                All
+            </button>
+            <button onClick={ () => { props.changeFilter("active") } }>
+                Active
+            </button>
+            <button onClick={ () => { props.changeFilter("completed") } }>
+                Completed
+            </button>
+        </div>
+    </div>
+}
