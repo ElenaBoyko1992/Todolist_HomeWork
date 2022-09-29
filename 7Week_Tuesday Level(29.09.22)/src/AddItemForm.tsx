4e70f7c36d26da2403eb060e9e2938b3526b1f4d
ElenaBoyko1992 <Bionka@inbox.ru>
2022-09-29 23:40:52 +0300

    29.09.2022

---
+++ 7Week_Tuesday Level(29.09.22)/src/AddItemForm.tsx
@@ -0,0 +1,50 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import {IconButton, TextField} from "@material-ui/core";
+import QueueIcon from '@material-ui/icons/Queue';
+
+type AddItemFormPropsType = {
+    addItem: (title: string) => void
+}
+
+export function AddItemForm(props: AddItemFormPropsType) {
+
+    let [title, setTitle] = useState("")
+    let [error, setError] = useState<string | null>(null)
+
+    const addItem = () => {
+        if (title.trim() !== "") {
+            props.addItem(title);
+            setTitle("");
+        } else {
+            setError("Title is required");
+        }
+    }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null);
+        if (e.charCode === 13) {
+            addItem();
+        }
+    }
+
+    return <div>
+        <TextField label="Type text"
+                   value={title}
+                   onChange={onChangeHandler}
+                   onKeyPress={onKeyPressHandler}
+                   error={!!error}
+                   variant={"outlined"}
+                   size={"small"}
+                   helperText={error && "Title is required!"}
+
+        />
+
+        <IconButton onClick={addItem} color="primary">
+            <QueueIcon/>
+        </IconButton>
+    </div>
+}
