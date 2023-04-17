3b88dfb97a620737feb4dbd063f286918664126c
ElenaBoyko1992 <Bionka@inbox.ru>
2023-04-17 22:11:55 +0300

    17.04.2023

---
+++ 19Week_Thursday Level(17.04.23)/src/common/utils/handle-server-app-error.ts
@@ -0,0 +1,12 @@
+import {Dispatch} from 'redux';
+import {appActions} from 'app/app.reducer';
+import {ResponseType} from '../types';
+
+
+export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch, showError: boolean = true
+) => {
+    if (showError) {
+        dispatch(appActions.setAppError({error: data.messages.length ? data.messages[0] : 'Some error occurred'}))
+    }
+    dispatch(appActions.setAppStatus({status: 'failed'}))
+}
